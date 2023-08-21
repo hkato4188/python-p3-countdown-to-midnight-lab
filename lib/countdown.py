c29db0b186116c7817d5c4039272a9034ed51511
Hiroki Kato <hirokikato1@gmail.com>
2023-08-21 16:15:33 -0700

    completes lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+from time import sleep
+
+def countdown(integer):
+
+    while integer:
+        print(f'{integer} SECOND(S)!')
+        integer -= 1
+
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(integer):
+
+    while integer:
+        print(f'{integer} SECOND(S)!')
+        integer -= 1
+        sleep(1)
+
+    print('HAPPY NEW YEAR!')
